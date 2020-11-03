6fe7b809a52e627758f6bf67a142cab396466047
RKRedfern <rkredfern@gmail.com>
2020-11-02 21:25:41 -0500

    ruby-oo-complex-objects-putting-the-pieces-together.rb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
 # Make your shoe class here!
+
+class Shoe
+
+    def initialize(brand)
+        @brand = brand 
+    end
+
+    def brand 
+        @brand
+    end
+
+    # def size=(size)
+    #     @size = size
+    # end
+
+    # def size 
+    #     @size
+    # end
+
+    # def color=(color)
+    #     @color = color
+    # end
+
+    # def color 
+    #     @color
+    # end
+
+    attr_accessor :material, :condition, :size, :color
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
